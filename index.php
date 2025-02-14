0aef98fdc1f748c68d5ef0e69cc41c169ff236c8
Ajay Kumar Arya <ajaykumararya963983@gmail.com>
2025-02-14 23:46:43 +0530

    update data

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ date_default_timezone_set('Asia/Kolkata');
  * @since	Version 1.0.0
  * @filesource
  */ 
-define('ENVIRONMENT', isset($_SERVER['CI_ENV']) ? $_SERVER['CI_ENV'] : 'development');
+define('ENVIRONMENT', isset($_SERVER['CI_ENV']) ? $_SERVER['CI_ENV'] : 'production');
 /*
  *---------------------------------------------------------------
  * ERROR REPORTING
