ca458694467add08db24a44a100f40618f2383ad
Ajay Kumar Arya <ajaykumararya963983@gmail.com>
2025-02-15 10:56:47 +0530

    update theme btn

--- engine/config/email.php
+++ engine/config/email.php
@@ -2,8 +2,8 @@
 $config['protocol'] = 'smtp';
 $config['smtp_host'] = 'smtp.gmail.com';
 $config['smtp_port'] = 465; // Use the appropriate port for your SMTP server
-$config['smtp_user'] = 'bizknowindia@gmail.com';
-$config['smtp_pass'] = 'cyaz rwlb bhfw kmxx';
+$config['smtp_user'] = 'rainboweduzone.fzd@gmail.com';
+$config['smtp_pass'] = 'ndib aooi fatf dsbh';
 
 $config['smtp_crypto'] = 'ssl'; // Use 'ssl' or 'tls' based on your SMTP server configuration
 
